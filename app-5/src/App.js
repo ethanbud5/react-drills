5ffbb1659c88114db4ba91303cb3ee5c16b8d5d0
Ethan Sanders <ethanedu5@gmail.com>
2018-09-28 15:23:29 -0500

    App-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from "./Image"
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      imgUrl:""
+    }
+    this.imgChange = this.imgChange.bind(this)
+  }
+  imgChange(event){
+    let {name,value} = event.target;
+    this.setState({
+      [name]:value
+    })
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <input type="text" placeholder="Image URL" name="imgUrl" onChange={this.imgChange}/>
+        <div>
+          <Image img={this.state.imgUrl}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ app-5/src/Image.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+
+function Image(props){
+    return(
+        <img src={props.img}/>
+    )
+}
+
+export default Image;
