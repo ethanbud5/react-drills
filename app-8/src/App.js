66c6dabc6a49da65c4c600215a95695fc7dbd1e4
Ethan Sanders <ethanedu5@gmail.com>
2018-09-30 01:25:40 -0500

    app-8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      planet:{
+        name:"",
+        population:"",
+        climate:""
+      }
+    }
+  }
+
+  componentDidMount(){
+    //https://opinionated-quotes-api.gigalixirapp.com/v1/quotes?rand=t&n=1
+    axios.get("https://swapi.co/api/planets/8/")
+      .then(res=>{
+        this.setState({
+          planet:res.data
+        })
+      })
+  }
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>Star Wars Planet</h1>
+        <div>
+          <h2>Name: {this.state.planet.name}</h2>
+          <h4>Population: {this.state.planet.population}</h4>
+          <h4>Climate: {this.state.planet.climate}</h4>
+          <div>
+            <h2>Residents of {this.state.planet.name}</h2>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
